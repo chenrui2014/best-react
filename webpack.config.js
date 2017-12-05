7c3bed4b0fe2f7bf0b9c789a5371d0fc92fe0318
chenrui2014 <chenruixiang2009@163.com>
2017-12-06 00:24:27 +0800

    添加副作用依赖

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,21 @@
-
+const path = require('path');
 module.exports = {
+
+    entry:[
+        './src/index.js'
+    ],
+    output: {
+        filename: "bound.js",
+        path: path.resolve(__dirname,"dist")
+    },
+    module: {
+        rules: [
+            {
+                test:{
+
+                }
+            }
+        ]
+    }
 
 }
